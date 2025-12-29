52481aa506cc6c19dd6f6d9ca78c4fed2e1abcc5
kryptoleaf90 <harjot7659@gmail.com>
2025-12-29 16:46:51 +0530

    first commit

---
+++ CLASSIC_RPS.py
@@ -0,0 +1,24 @@
+import random
+
+response = ("rock","paper","scissors")
+
+
+
+is_running = True
+print("-----CLASSIC RPS-----")
+while is_running:
+    player = input("YOUR CHOICE:").lower()
+    computer = random.choice(response)
+    if player == computer:
+        print("TIE!")
+    elif player == "rock" and computer == "scissors":
+        print("YOU WIN!!")
+    elif player == "scissors" and computer == "paper":
+        print("YOU WIN!!")
+    elif player == "paper" and computer == "rock":
+        print("YOU WIN!!")
+    else:
+        print("YOU LOOSE!!")
+    again=input("play again?(y/n):")
+    if again!="y":
+        is_running=False
