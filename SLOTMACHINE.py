b02e270529669a93e2dcd5964b7a820b55bfb642
kryptoleaf90 <harjot7659@gmail.com>
2025-12-30 17:08:14 +0530

    UPDATE01

---
+++ SLOTMACHINE.py
@@ -0,0 +1,21 @@
+import random
+
+
+def slot_choice():
+    pass
+
+def slot_print():
+    pass
+
+def pay_out():
+    pass
+
+def main():
+    balance = input("ENTER BALANCE: ")
+    while not balance.isdigit():
+        balance = input("ENTER VALID AMOUNT: ")
+    balance = int(balance)
+
+    
+
+main()
